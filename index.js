48a47d9ad35f46eede7498bed0adcd02e813002b
thinh185 <thinhphoho01@gmail.com>
2021-01-11 16:01:39 +0700

    update

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ export const initZoomSdk = (domain, clientKey, clientSecret) => {
 export const ZoomJoinMeetingWithPassword = (data) => {
   ZoomModule.joinMeeting(data);
 };
+export const ZoomLeaveCurrentMeeting = () => {
+  ZoomModule.leaveCurrentMeeting();
+};
+
 export const onOffAudioZoom = () => {
   ZoomModule.onOffMyAudio();
 };
